06a4381d945b1f114a10c2d3e2a7758e3c89aa01
Moises Sanabria <sesmoi.data@gmail.com>
2020-09-21 05:39:10 -0400

    subscribe args listeners array .push listener and listeners.filter((l) => l !== listener) unsubscribe

--- index.js
+++ index.js
@@ -6,10 +6,23 @@ function createStore() {
   // 4. Update the state
 
   let state;
+  let listeners = [];
 
   const getState = () => state;
+  const subscribe = (listener) => {
+    listeners.push(listener);
+    return () => {
+      listeners = listeners.filter((l) => l !== listener);
+    };
+  };
 
   return {
     getState,
+    subscribe,
   };
 }
+
+const store = createStore();
+store.subscribe(() => {
+  () => {};
+});
