3267795726d28b06dbbfe164d4d5d36f29706af1
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-01 07:26:48 -0400

    useRef input react-redux, useDispatch handleAddDeleteGoal, useSelector((state) .goals), imp List comp

--- src/components/Goals/index.js
+++ src/components/Goals/index.js
@@ -1,13 +1,18 @@
-import React from "react";
-import { connect } from "react-redux";
-import PropTypes from "prop-types";
+import React, { useRef } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { handleAddGoal, handleDeleteGoal } from "../../actions/goals";
 
-function Goals({ dispatch, goals }) {
+export default function Goals() {
+  const input = useRef("");
+  const dispatch = useDispatch();
+  const goals = useSelector((state) => state.goals);
+
   const addItem = (e) => {
     e.preventDefault();
 
-    dispatch(handleAddGoal(this.input.value, () => (this.input.value = "")));
+    dispatch(
+      handleAddGoal(input.current.value, () => (input.current.value = ""))
+    );
   };
 
   const removeItem = (goal) => {
@@ -17,23 +22,10 @@ function Goals({ dispatch, goals }) {
   return (
     <div>
       <h1>Goals</h1>
-      <input
-        type="text"
-        placeholder="Add Goal"
-        ref={(input) => (this.input = input)}
-      />
+      <input type="text" placeholder="Add Goal" ref={input} />
       <button onClick={addItem}>Add Goal</button>
 
       <List items={goals} remove={removeItem} />
     </div>
   );
 }
-
-export default connect((state) => ({
-  goals: state.goals,
-}))(Goals);
-
-Goals.propTypes = {
-  dispatch: PropTypes.func.isRequired,
-  goals: PropTypes.array.isRequired,
-};
